5c927cd0944a14466f13d7f0bcaa7a56b09ec187
0291149 <halmnop@gmail.com>
2017-04-03 16:17:23 -0300

    Needing to finish the functions of update and delete. Still needs testing.

--- backend/controllers/EventsController.js
+++ backend/controllers/EventsController.js
@@ -3,23 +3,70 @@
  */
 
 module.exports.index = function(req,res,next){
-    console.log("In the GET method");
+    req.getConnection(function(err,connection){
+        if(err) return next(err);
+
+        var query = connection.query('SELECT * FROM Event;', [], function(err, results){
+            if (err) return next(err);
+
+            res.json(results);
+        });
+
+        console.log(query.sql);
+
+    });
 };
 
 module.exports.show = function(req,res,next){
-    console.log("In the GET method on id");
+    var eventID = req.params.eventID;
+
+    req.getConnection(function(err, connection){
+        if (err) return next(err);
+
+        var query = connection.query('SELECT * FROM Event WHERE eventId = ?', [eventID], function(err, results){
+
+            if(err) return next(err);
+
+            res.json(results[0]);
+
+        });
+
+        console.log(query.sql);
+
+    });
+
 };
 
 module.exports.store = function(req,res,next){
+
     var input = req.body;
 
-    console.log(input)
+    req.getConnection(function(err, connection){
+
+        if(err) return next(err);
+
+        var query = connection.
+        query('INSERT INTO Event (title, startDate, endDate, isAllDay, description, location, userId) VALUES (?,?,?,?,?,?,?);',
+            [input.title, input.startDate, input.endDate, input.isAllDay, input.description, input.location, input.userId],
+        function(err, results){
+
+            if(err) return next(err);
+
+            res.json({message: 'Employee Created'});
+
+        });
+
+        console.log(query.sql);
+
+    });
 
 };
 
 module.exports.update = function(req,res,next){
     var eventID = req.params.eventID;
     var input = req.body;
+
+    //req.getConnection(function)
 
     console.log(input);
 };
